b6f22e84c4f852ba86fdb048201b9d9d7625c96e
FilipKochan <filda.koch@gmail.com>
2022-01-24 21:14:52 +0100

    add db to env

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
-
+require("dotenv/config");
 const con = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "root",
-  database: "auticka",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE_NAME,
 });
 con.connect((err) => {
   if (err) throw err;
